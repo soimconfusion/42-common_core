109b8cfe59dd2a65e2cd5eaf1fcda6c5fa3e5b28
Ana Pereira <142814914+soimconfusion@users.noreply.github.com>
2024-12-11 17:02:59 +0000

    Add files via upload

--- printf/test_printf.c
+++ printf/test_printf.c
@@ -1,46 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_printf.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anferrei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 16:58:10 by anferrei          #+#    #+#             */
+/*   Updated: 2024/12/07 17:11:58 by anferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-#include "ft_printf.h" // Include your custom header file
+#include "header/ft_printf.h"
 
-int main(void)
+int	main(void)
 {
-    int orig;
-    int ft;
+	int	orig;
+	int	ft;
+	int	x;
 
-    // Testing %c
-    orig = printf("orig: %c\n", 'A');
-    ft = ft_printf("ft  : %c\n", 'A');
-    printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
-
-    // Testing %s
-    orig = printf("orig: %s\n", "Hello, world!");
-    ft = ft_printf("ft  : %s\n", "Hello, world!");
-    printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
-
-    // Testing %p
-    int x = 42;
-    orig = printf("orig: %p\n", &x);
-    ft = ft_printf("ft  : %p\n", &x);
-    printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
-
-    // Testing %d and %i
-    orig = printf("orig: %d, %i\n", 123, -456);
-    ft = ft_printf("ft  : %d, %i\n", 123, -456);
-    printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
-
-    // Testing %u
-    orig = printf("orig: %u\n", 3000000000U);
-    ft = ft_printf("ft  : %u\n", 3000000000U);
-    printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
-
-    // Testing %x and %X
-    orig = printf("orig: %x, %X\n", 48879, 48879);
-    ft = ft_printf("ft  : %x, %X\n", 48879, 48879);
-    printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
-
-    // Testing %%
-    orig = printf("orig: %%\n");
-    ft = ft_printf("ft  : %%\n");
-    printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
-
-    return 0;
+	x = 42;
+	orig = printf("orig: %c\n", 'A');
+	ft = ft_printf("ft  : %c\n", 'A');
+	printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
+	orig = printf("orig: %s\n", "Hello, world!");
+	ft = ft_printf("ft  : %s\n", "Hello, world!");
+	printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
+	orig = printf("orig: %p\n", &x);
+	ft = ft_printf("ft  : %p\n", &x);
+	printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
+	orig = printf("orig: %d, %i\n", 123, -456);
+	ft = ft_printf("ft  : %d, %i\n", 123, -456);
+	printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
+	orig = printf("orig: %u\n", 3000000000U);
+	ft = ft_printf("ft  : %u\n", 3000000000U);
+	printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
+	orig = printf("orig: %x, %X\n", 48879, 48879);
+	ft = ft_printf("ft  : %x, %X\n", 48879, 48879);
+	printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
+	orig = printf("orig: %%\n");
+	ft = ft_printf("ft  : %%\n");
+	printf("Return values: Orig = %d, FT = %d\n\n", orig, ft);
+	return (0);
 }
